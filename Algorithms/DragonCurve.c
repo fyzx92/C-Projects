3261cbfafc17b05c26e059e9ba888e3030a71bc9
fyzx92 <brycebrgss@yahoo.com>
2018-09-23 12:27:34 -0700

    beginning of programming

---
+++ Algorithms/DragonCurve.c
@@ -0,0 +1,19 @@
+/*
+* Authored by Bryce Burgess
+*
+* Dragon curve fractal
+*/
+
+// start with V
+// for each vertex replace adjacent line with corresponding V
+
+typedef struct Corner {
+
+} Corner;
+
+typedef struct Fractal {
+    
+} Fractal;
+
+Corner *pv;
+Corner v = *pv;
